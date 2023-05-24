bb15cb98cfe66a9c7a1bc6e0e7ecb492c9f183dc
Andres Jurado <andresymendoza@gmail.com>
2023-05-24 01:56:57 -0500

    Modelos carreras

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/datos',function(){
+    return view('datos');
+})->name('datos');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
